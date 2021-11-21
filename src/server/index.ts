c5059fa8de60ccdb5fb755dea84adb340d2be203
Vamshi Krishna Pothuka <pvamshi@gmail.com>
2021-11-21 23:24:24 +0100

    refactor

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,51 +7,35 @@ type Data = {
   books: Book[];
 };
 
-const adapter = new JSONFile<Data>(
-  new URL("./db.json", import.meta.url).pathname
-);
-const db = new Low<Data>(adapter);
-
-/*
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
- `;
-
- */
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers: Resolvers = {
-  Query: {
-    books: async () => {
-      await db.read();
-      db.data = db.data || { books: [] };
-      return db.data.books;
-    },
-  },
+const initDB = async () => {
+  const adapter = new JSONFile<Data>(
+    new URL("./db.json", import.meta.url).pathname
+  );
+  const db = new Low<Data>(adapter);
+  await db.read();
+  return db;
 };
 
-const typeDefs = importSchema("src/server/schema.graphql");
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const initApolloServer = (db: Low<Data>) => {
+  // Resolvers define the technique for fetching the types defined in the
+  // schema. This resolver retrieves books from the "books" array above.
+  const resolvers: Resolvers = {
+    Query: {
+      books: async () => {
+        db.data = db.data || { books: [] };
+        return db.data.books;
+      },
+    },
+  };
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }: { url: string }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  const typeDefs = importSchema("src/server/schema.graphql");
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({ typeDefs, resolvers });
+  return server.listen();
+};
+initDB()
+  .then(initApolloServer)
+  .then(({ url }: { url: string }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
